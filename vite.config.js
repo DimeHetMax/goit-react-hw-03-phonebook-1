a1316dc817b8fc0ebb0a35df0179ca5c3437a633
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-08-20 22:25:48 +0300

    config

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 export default {
   base: '/goit-react-hw-03-phonebook-1/',
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@components': '/src/components', 
+    },
+  },
 };
