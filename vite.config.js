c14e6545ac121809c8abfe88767b473b17b65772
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-08-20 14:07:31 +0300

    Update vite.config.js
    vite.config

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-// https://vitejs.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
 export default {
-  base: '/goit-react-hw-03-phonebook-1/', // Change this to match your repository name
+  base: '/goit-react-hw-03-phonebook-1/',
+  plugins: [react()],
 };
