5ef6912fdef08be4e04cf6f9e7048684de4d9342
DimeHetMax <86876206+DimeHetMax@users.noreply.github.com>
2023-08-20 13:51:09 +0300

    finishing
    adding componentdidMount/ DidUpdate
    adding prototypes to components

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,19 @@
 import React from 'react'
+import { ContactForm } from './ContactForm/ContactForm'
+import { Filter } from './Filter/Filter'
+import { ContactList } from './ContactList/ContactList'
 import { nanoid } from 'nanoid'
-
 import './App.css'
 
 class App extends React.Component {
   state = {
     contacts: [],
     filter: '',
-    name: '',
-    number: '',
+    name: "",
+    number:"",
+  }
+  handleInput=(event)=>{
+    this.setState({[event.target.name]: event.target.value})
   }
   handleForm = (event) =>{
     event.preventDefault()
@@ -18,89 +23,52 @@ class App extends React.Component {
       alert(`${existingContacts.name} exists in your contacts `)
       return
     }
-  
     const modelId = nanoid() 
     this.setState(prevState =>{
+      console.log(prevState);
       return({
         contacts:[... prevState.contacts, {name: prevState.name, number: prevState.number, id: modelId}],
-        name: "",
-        number:"",
+       name:"",
+       number:"",
       })
     })
     event.target.reset();
-  }
-  handleInput=(event)=>{
-    console.log(event.target.value);
-    console.log(event.target.name);
-    this.setState({[event.target.name]: event.target.value})
   }
   handleFilteredContacts = (event) =>{
     console.log(event.target.value);
     this.setState({filter: event.target.value.toLowerCase()})
   }
+  deleteContact =(id)=>{
+    const {contacts} = this.state
+    const deletedContact = contacts.filter(contact => contact.id !== id)
+    this.setState({contacts: deletedContact})
+  }
+  componentDidMount(){
+    const savedContacts = localStorage.getItem("contacts");
+    if (savedContacts) {
+      this.setState({ contacts: JSON.parse(savedContacts) });
+    }
+  }
+  componentDidUpdate(prevProps, prevState){
+    const {contacts} = this.state
+    if(prevState.contacts !== contacts){
+      localStorage.setItem("contacts", JSON.stringify(contacts))
+    }
+  }
   render(){
-    const {contacts, name, filter} = this.state;
+    const {contacts, name, number, filter} = this.state;
     const filteredContacts = contacts.filter(contact => contact.name.toLowerCase().includes(filter.toLowerCase()));
-    console.log(filteredContacts);
     
     return(
       <div>
         <h1>Phone Book</h1>
-        <form onSubmit={this.handleForm}>
-        <label >
-        Name
-          <input
-            type="text"
-            name="name"
-            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
-            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
-            required
-            onChange = {this.handleInput}
-            value={name}
-          />
-        </label>
-        <label >
-        Number
-          <input
-            type="tel"
-            name="number"
-            pattern="\+?\d{1,4}?[-.\s]?\(?\d{1,3}?\)?[-.\s]?\d{1,4}[-.\s]?\d{1,4}[-.\s]?\d{1,9}"
-            title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
-            required
-            onChange = {this.handleInput}
-          />
-        </label>
-       
-            <button type="submit">Add contacts</button>
-        </form>
+        <ContactForm handleForm={this.handleForm} handleInput={this.handleInput} name={name} number={number}/>
         <h2>Contacts</h2>
-        <div>
-          <label>
-            Find contacts by name
-            <input type="text" onChange={this.handleFilteredContacts}/>
-          </label>
-        </div>
-        {filter !== "" ? 
-        ( <div>
-          <ul>
-            {filteredContacts.map(contact => {
-              return(<li key={contact.id}>{contact.name}: {contact.number}</li>)})}
-          </ul>
-        </div>): 
-        (contacts.length > 0 &&  (
-          <div>
-            <ul>
-              {contacts.map(contact => {
-                return(<li key={contact.id}>{contact.name}: {contact.number}</li>)})}
-            </ul>
-          </div>
-        ))}
-
-
+        <Filter handleFilteredContacts={this.handleFilteredContacts}/> 
+        <ContactList contacts={contacts} filter={filter} filteredContacts={filteredContacts} deleteContact={this.deleteContact}/>
       </div>
     )
   }
-  
 }
 
 export default App;
---
+++ src/ContactList/ContactList.jsx
@@ -0,0 +1,44 @@
+import React from "react";
+import PropTypes from 'prop-types';
+export class ContactList extends React.Component{
+    render(){
+        const {contacts, filter, filteredContacts, deleteContact} = this.props;
+        return(
+            <div> 
+                {filter !== "" ? 
+                ( <div>
+                    <ul>
+                    {filteredContacts.map(contact => {
+                        return(<li key={contact.id}>{contact.name}: {contact.number} <button className="delete-contact" style={{padding:0}} onClick={()=>deleteContact(contact.id)}>Delete</button></li>)})}
+                    </ul>
+                </div>): 
+                (contacts.length > 0 &&  (
+                    <div>
+                    <ul>
+                        {contacts.map(contact=> {
+                        return(<li key={contact.id}>{contact.name}: {contact.number} <button className="delete-contact" style={{padding:0}} onClick={()=>deleteContact(contact.id)}>Delete</button></li>)})}
+                    </ul>
+                    </div>
+                ))}
+            </div>
+        )
+    }
+}
+ContactList.protoTypes ={
+    contacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            number: PropTypes.number.isRequired,
+            id: PropTypes.string.isRequired,
+          }).isRequired,
+    ),
+    filteredContacts: PropTypes.arrayOf(
+        PropTypes.shape({
+            name: PropTypes.string.isRequired,
+            number: PropTypes.number.isRequired,
+            id: PropTypes.string.isRequired,
+          }).isRequired,
+    ),
+    filter: PropTypes.string.isRequired,
+    deleteContact: PropTypes.func.isRequired,
+}
